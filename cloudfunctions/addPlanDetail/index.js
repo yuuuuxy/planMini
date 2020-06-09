a6544dae2c3a227fcb2ae6de63c985d5c09e1f98
Ariel <985472538@qq.com>
2020-06-09 20:46:19 +0800

    添加功能改为先删后插，合并修改功能

--- cloudfunctions/addPlanDetail/index.js
+++ cloudfunctions/addPlanDetail/index.js
@@ -5,7 +5,15 @@ const db = cloud.database()
 const _ = db.command
 // 云函数入口函数
 exports.main = async (event, context) => {
-  let id =  event.id;
+  let id = event.id;
+  const res1 = await cloud.callFunction({
+    // 要调用的云函数名称
+    name: 'deletePlanDetail',
+    data: {
+      currId: id,
+      detailid: event.obj.detailid,
+    }
+  })
   const res = await db.collection('weight').doc(id).update({
     data: {
       // lastModifydate: '123',
